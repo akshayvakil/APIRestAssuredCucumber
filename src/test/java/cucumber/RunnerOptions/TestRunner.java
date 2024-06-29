b9092626db2344e92725e1dc6200e89179ceb868
akshayvakil <157507818+akshayvakil@users.noreply.github.com>
2024-06-29 19:13:07 +0530

    Reports added check notes for reports

--- src/test/java/cucumber/RunnerOptions/TestRunner.java
+++ src/test/java/cucumber/RunnerOptions/TestRunner.java
@@ -12,13 +12,25 @@ import io.cucumber.junit.CucumberOptions;
  * Before 5.0.0 mvn test -Dcucumber.options="--tags @AddPlace"'
  * After  5.0.0 mvn test -Dcucumber.filter.tags="@AddPlace"
  * @author AKSHAY
- *
+ * For Reporting use pom.xml from link
+ * https://github.com/damianszczepanik/maven-cucumber-reporting/ "use from comments and check version
+ * Do not use pom.xml from project
  */
 
 @RunWith(Cucumber.class)
 //cucumber option annotation 
 //              (path of package in which features file is present, packageName where step definition is present)
-@CucumberOptions(features="src/test/java/features",glue="stepDefinition",tags="@DeletePlace")
+
+@CucumberOptions(
+		features="src/test/java/features",
+		glue="stepDefinition",
+		plugin = "json:target/jsonReports/cucumber-report.json")
+//Below plugin to see ACtual scenario in console
+//@CucumberOptions(features="src/test/java/features",glue="stepDefinition",plugin = {"pretty", "html:target/cucumber-reports"})
+
+//@CucumberOptions(features="src/test/java/features/placeValidation.feature",glue="stepDefinition",tags="@AddPlace") // To run only one feature file
+
+
 // Remove TAG to run both scenarios from feature file
 //to add multuple tags follow --> tags="@AddPlace or @DeletePlace"
 public class TestRunner {
